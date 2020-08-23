7c431b4adfece8e880417a3bfea61b11447ab713
Nanda Vikas <nandavikas858@gmail.com>
2020-08-23 12:44:48 +0530

    refined the location search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     console.log(latitude, longitude)
-    const url = `http://api.weatherstack.com/current?access_key=bf59123a36c9a99883a40f7900067980&query=${latitude,longitude}&units=m`
+    const url = 'http://api.weatherstack.com/current?access_key=bf59123a36c9a99883a40f7900067980&query='+ latitude+ ',' +longitude + '&units=m'
     const options = {
         url,
         json: true
@@ -13,10 +13,11 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service!',undefined)
         }
         else if (body.error) {
-            // console.log(response.body.error)
+            // console.log(body.error)
             callback('Unable to find location',undefined)
         }
         else {
+            // console.log(body)
             callback(undefined,{weather: `Weather Description: ${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out but it feels like ${body.current.feelslike} degrees`, windSpeed: body.current.wind_speed, pressure: body.current.pressure})
         }
     })
